d7c833c1b4e78521337d37779c6e6e725a2b105d
Herica Cadoni <hericacadoni@gmail.com>
2022-01-24 11:37:30 -0300

    Add project files.

---
+++ HtmlProcessor/Program.cs
@@ -0,0 +1,34 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace HtmlProcessor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("C:/Users/heric/Downloads/TesteConsole/listadeacoes.txt");
+                var doc = new HtmlWeb().Load("https://www.infomoney.com.br/cotacoes/empresas-b3/");
+                var acoes = doc.DocumentNode.Descendants().Where(d => d.Attributes.Contains("class") && d.Attributes["class"].Value.Contains("strong"));
+                foreach (var acao in acoes)
+                {
+                    sw.Write(acao.InnerText);
+                }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Verifique a pasta Downloads, a saída de seu programa está lá.");
+            }
+
+        }
+    }
+}
